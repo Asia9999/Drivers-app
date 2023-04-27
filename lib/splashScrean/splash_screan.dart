5b705ee9c80a7411735cd138aac17ff2a7152d84
Asia9999 <asia272526@gmail.com>
2023-04-27 16:16:52 +0300

    add project files

---
+++ lib/splashScrean/splash_screan.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+
+import 'package:drivers_app/authentication/login-screen.dart';
+import 'package:drivers_app/authentication/signup_screen.dart';
+import 'package:drivers_app/global/global.dart';
+import 'package:drivers_app/mainScreens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+class MySplashScrean extends StatefulWidget
+{
+  const MySplashScrean({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScrean> createState() => _MySplashScreanState();
+}
+
+class _MySplashScreanState extends State<MySplashScrean> {
+  //display timer
+  startTimer(){
+    Timer(const Duration(seconds: 3),()async {
+      if(await fAuth.currentUser != null)
+        {
+          currentFirebaseUser=fAuth.currentUser;
+          // send user to home screen
+          Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
+
+        }
+      else
+        {
+          // send user to home screen
+          Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
+
+        }
+       });
+  }
+  
+  @override
+  //executed atomatically
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    
+    startTimer(); //calling method
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("images/Logo.jpeg"),
+            const SizedBox(height: 10,),
+
+           const Text(
+              "Welcome to Group Ride Hailing",
+              style: TextStyle(
+                fontSize: 20,
+                  color: Colors.black,
+                  fontFamily: 'Sigmar',
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            const Text(
+              "                                     "
+                  "  A ride is just a click away Share "
+                  "  your ride at the lowest cost ",
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontFamily: 'Lobster',
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
